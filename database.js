821f16640f014b7673e02776c2adc41f51e5e0c4
leo-Saf <leo.safiah@gmail.com>
2024-11-30 13:40:57 +0100

    Update database.js

--- database.js
+++ database.js
@@ -6,7 +6,11 @@ import { Storage } from '@google-cloud/storage';
 
 // Initialize Firebase Admin with service account credentials
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const serviceAccount = JSON.parse(fs.readFileSync('//Users/hasansafiah/Desktop/Keys/speachai-b5ce2-firebase-adminsdk-odts8-8809efb41f.json', 'utf8'));
+=======
+const serviceAccount = JSON.parse(fs.readFileSync('/Users/hasansafiah/Desktop/Keys/speachai-b5ce2-firebase-adminsdk-odts8-8809efb41f.json', 'utf8'));
+>>>>>>> Stashed changes
 =======
 const serviceAccount = JSON.parse(fs.readFileSync('/Users/hasansafiah/Desktop/Keys/speachai-b5ce2-firebase-adminsdk-odts8-8809efb41f.json', 'utf8'));
 >>>>>>> Stashed changes
